0a6c93cf8ff34c13aa1aee8d38cfe301c498ca0d
dell <1716200680@qq.com>
2017-09-12 14:09:16 +0800

    汪炎

--- app/src/main/java/com/example/dell/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/dell/gitdemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import static android.R.attr.x;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
